a44705ac5ed045b885114035abefcec166293df5
oliburt <oliburt@gmail.com>
2019-11-19 17:00:42 +0000

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Todo from './Todo'
+import ACTIONS from '../../reducers/actions'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  renderTodos = () => this.props.todos.map((todo) => <Todo delete={this.props.delete} key={todo.id} text={todo.text} id={todo.id}/>)
 
   render() {
     return(
@@ -15,10 +16,14 @@ class TodosContainer extends Component {
   }
 };
 
-mapStateToProps = state => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => ({
+  delete: todoText => dispatch({type: ACTIONS.DELETE_TODO, payload: todoText})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,11 +1,19 @@
+import actions from './actions.js'
+import uuid from "uuid";
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
   console.log(action)
   switch (action.type) {
-    case 'ADD_TODO':
+    case actions.ADD_TODO:
 
-      return { todos: state.todos.concat(action.payload.text) };
+      return { todos: state.todos.concat({id: uuid(), text: action.payload.text}) };
+  
+
+    case actions.DELETE_TODO:
+
+      return { todos: state.todos.filter(todo => todo.id !== action.payload) };
 
     default:
       return state;
